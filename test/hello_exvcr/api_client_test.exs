11366c8078141b139e099edbcb37ed6a9d02980d
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-10-06 17:53:22 +0900

    Generate cassette path based on test file

--- test/hello_exvcr/api_client_test.exs
+++ test/hello_exvcr/api_client_test.exs
@@ -4,11 +4,29 @@ defmodule HelloExvcr.ApiClientTest do
 
   doctest HelloExvcr.ApiClient
 
-  test "get_todo" do
-    use_cassette "api_client_test_get_todo" do
+  test "get_todo", context do
+    use_cassette_with_context(context, fn _context ->
       {:ok, response} = HelloExvcr.ApiClient.get_todo(123)
 
       assert %{"title" => "esse et quis iste est earum aut impedit"} = response.body
+    end)
+  end
+
+  def use_cassette_with_context(context,test_content_fn) do
+    # See https://hexdocs.pm/exvcr/ExVCR.Mock.html#use_cassette/2
+    use_cassette(cassette_path_from_context(context)) do
+      test_content_fn.(context)
     end
+  end
+
+  def use_cassette_with_context(context, vcr_options, test_content_fn) do
+    # See https://hexdocs.pm/exvcr/ExVCR.Mock.html#use_cassette/3
+    use_cassette(cassette_path_from_context(context), vcr_options) do
+      test_content_fn.(context)
+    end
+  end
+
+  def cassette_path_from_context(context) do
+    Path.join(Macro.underscore(context.case), to_string(context.test))
   end
 end
